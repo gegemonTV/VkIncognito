05786b49285e938b6176ad34144f1ddb1d23e355
Vladimir <51519538+gegemonTV@users.noreply.github.com>
2021-04-04 16:21:36 +0300

    updated main

--- main.c
+++ main.c
@@ -6,12 +6,17 @@ int main(int argc, char **argv)
     gtk_init(&argc, &argv);
 
     GtkWidget *window;
+    GtkWidget *username_label, *passwd_label;
+    GtkWidget *username_entry, *passwd_entry;
+    GtkWidget *log_in_button;
+    GtkWidget *horizontal_box_username, *horizontal_box_passwd;
+    GtkWidget *vertical_box;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     gtk_window_set_title(GTK_WINDOW(window), "Vk Incognito");
     gtk_window_set_resizable(GTK_WINDOW(window), gtk_true());
-    gtk_window_resize(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_widget_show(window);
